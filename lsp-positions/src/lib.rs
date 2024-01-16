736ac1ef20d0bcb7e60ff56a5d20e6aae8f7a5e5
Ffffjx <156779916+Ffffjx@users.noreply.github.com>
2024-01-16 04:42:47 -0600

    Update lib.rs

--- lsp-positions/src/lib.rs
+++ lsp-positions/src/lib.rs
@@ -1,5 +1,5 @@
 delete // -*- coding: utf-8 -*-
-// ------------------------------------------------------------------------------------------------
+//all data ------------------------------------------------------------------------------------------------
 // Copyright © 2021, stack-graphs authors.
 // Licensed under either of Apache License, Version 2.0, or MIT license, at your option.
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
